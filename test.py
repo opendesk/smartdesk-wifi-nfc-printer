c04c9a089089728381f1a2e6ece81898885c31fd
andrecp <andreprado88@gmail.com>
2015-04-13 11:46:09 -0300

    while heroku is off... simple script to post to arduino mailbox

--- test.py
+++ test.py
@@ -1,8 +1,22 @@
 import requests
+import time
 
-r = requests.get('http://www.smartdesk.cc/actions')
-json_data = r.json()
-if json_data:
-    for k,v in json_data.items():
-        url = 'http://arduinoyunone.local/mailbox/{0}'.format(''.join(v.split(',')[0]).split(':')[-1].replace('"','').replace(' ','%20'))
-        requests.post(url)
+def main():
+    current_data = False
+    while(1):
+        #r = requests.get('http://www.smartdesk.cc/new_value')
+        if 1:#r.json()['new_value'] == u'yes':
+            r = requests.get('http://www.smartdesk.cc/actions')
+            try:
+                json_data = r.json()
+                if current_data != json_data:
+                    current_data = json_data
+                    for k,v in json_data.items():
+                        url = 'http://localhost/mailbox/{0}'.format(''.join(v.split(',')[0]).split(':')[-1].replace('"','').replace(' ','%20'))
+                        print url
+            except ValueError:
+                pass
+        time.sleep(2)
+
+if __name__ == "__main__":
+    main()
