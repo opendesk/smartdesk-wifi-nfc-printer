6fe40490e0da3d168a6b5f06f247919bf403caea
andrecp <andreprado88@gmail.com>
2015-04-13 10:30:40 -0300

    script

--- test.py
+++ test.py
@@ -4,4 +4,5 @@ r = requests.get('http://www.smartdesk.cc/actions')
 json_data = r.json()
 if json_data:
     for k,v in json_data.items():
-        print ''.join(v.split(',')[0]).split(':')[-1]
+        url = 'http://arduinoyunone.local/mailbox/{0}'.format(''.join(v.split(',')[0]).split(':')[-1].replace('"','').replace(' ','%20'))
+        requests.post(url)
