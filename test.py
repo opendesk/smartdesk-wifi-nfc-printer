f156acfddcd1544230ef428f5e6f95d905d6c790
andrecp <andreprado88@gmail.com>
2015-04-13 14:16:57 -0300

    heroku back! Now we have control..

--- test.py
+++ test.py
@@ -5,23 +5,23 @@ import urllib
 def main():
     current_data = False
     while(1):
-        #r = requests.get('http://www.smartdesk.cc/new_value')
-        if 1:#r.json()['new_value'] == u'yes':
-            r = requests.get('http://www.smartdesk.cc/actions')
-            try:
+        try:
+            r = requests.get('http://www.smartdesk.cc/new_value')
+            if r.json()['has'] == u'yes':
+                r = requests.get('http://www.smartdesk.cc/actions')
                 json_data = r.json()
-                if current_data != json_data:
-                    current_data = json_data
-                    for k,v in json_data.items():
-                        encoded_url = urllib.quote_plus(''.join(v.split(',')[0]
-                            ).split(':')[-1].replace('"',''))
+                for k,v in json_data.items():
+                    encoded_url = urllib.quote_plus(''.join(v.split(',')[0]
+                        ).split(':')[-1].replace('"',''))
 
-                        url = 'http://localhost/mailbox/{0}'.format(encoded_url)
-                        requests.post(url)
+                    url = 'http://localhost/mailbox/{0}'.format(encoded_url)
+                    requests.post(url)
 
-            except (requests.exceptions.ConnectionError, ValueError):
-                pass
+        except (requests.exceptions.ConnectionError, ValueError):
+            pass
+
         time.sleep(2)
 
 if __name__ == "__main__":
+    time.sleep(5)
     main()
