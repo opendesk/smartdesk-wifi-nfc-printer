c66f168e812a60f11222157c3dace098bb80a7d2
andrecp <andreprado88@gmail.com>
2015-04-13 11:46:39 -0300

    Simple script to start python script in startup..

---
+++ startup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# launcher.sh
+# navigate to project directory because cant install request properly
+# run python script from there
+
+cd /root/kennethreitz-requests-a0632d8
+python test.py
